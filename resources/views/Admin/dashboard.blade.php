23bdbf24d3ed8831b50434b1df20939a033eb42f
SERLI30 <tiaa14596>
2025-06-02 03:54:11 +0800

    updatelagiiii

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -334,8 +334,10 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">VintageMuse</span>
-                                <img src="{{ asset('Admin\img\Vintages.png') }}" width="40" height="40">
+                                <span class="mr-2 d-none d-lg-inline text-warning small font-weight-bold">{{ Auth::user()->name }}</span>
+                                <div class="col-auto">
+                                            <i class="fas fa-user-shield fa-2x text-warning  "></i>
+                                        </div>
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
@@ -353,10 +355,12 @@
                                     Activity Log
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
+                                <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type='submit'><i 
+                            class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout</button>
+                        </form>
                             </div>
                         </li>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,9 +58,9 @@
                     @auth
                      <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
                     @else
-                    <li><a href="{{ route('login.html') }}">Login</a></li>
+                    <li><a href="{{ route('login') }}">Login</a></li>
                         @if (Route::has('register'))
-                        <li><a href="{{ route('register.html') }}">Register</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
                         @endif
                     @endauth
             @endif
